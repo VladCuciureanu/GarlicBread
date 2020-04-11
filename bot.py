692a14e2665383bc37cfc2d8e643cbccdc46ae5f
VladCuciureanu <vlad.c.cuciureanu@gmail.com>
2020-04-11 15:00:45 +0300

    Added sanity check at setup wizard

--- bot.py
+++ bot.py
@@ -35,6 +35,12 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_ready():
+    # Check if given guild id is valid
+    if config["guild"] not in bot.guilds:
+        os.remove("config.grlk")
+        raise ValueError("Bot doesn't belong to a guild with given ID.")
+        
+
     print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
     print(f'Successfully logged in and booted...!')
 
@@ -47,7 +53,13 @@ else:
     config["guild"] = int(input("Guild ID: "))
     pickle.dump(config, file=open("config.grlk", "wb"))
 
-
-bot.run(config["token"], bot=True, reconnect=True)
+try:
+    bot.run(config["token"], bot=True, reconnect=True)
+except discord.errors.LoginFailure as e:
+    print(e)
+    os.remove("config.grlk")
+    exit()
+except ValueError as e:
+    print(e)
+    exit()
 # TODO: fail-safe bot token and guild id connection (delete config and try again if wrong token)
-# TODO: install discord.py thru PIP
