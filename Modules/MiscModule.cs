896bb2d607e6adabeb3f5456e048f03a03f9ae74
VladCuciureanu <vlad.c.cuciureanu@gmail.com>
2021-03-19 18:00:37 +0200

    Reorganized commands and implemented Misc module

---
+++ Modules/MiscModule.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot;
+using Disqord.Events;
+using GarlicBread.Discord;
+using Humanizer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Qmmands;
+
+namespace GarlicBread.Modules
+{
+    [Name("Misc")]
+    public class MiscModule : GarlicBreadModuleBase
+    {
+        public ILogger<CoreModule> Logger { get; set; }
+        public IServiceProvider Services { get; set; }
+        
+        [Command("uwu")]
+        [Description("Definitely wont explain this.")]
+        [CommandCooldown(1, 3, CooldownMeasure.Seconds, CooldownType.User)]
+        public async Task Command_Uwu(string promptKey = null)
+        {
+            try
+            {
+                promptKey = promptKey ?? "?";
+
+                if (!Context.BotMember.GetPermissionsFor(Context.Channel as CachedTextChannel).SendMessages) return;
+
+                var prompt = Services.GetRequiredService<IConfiguration>().GetSection("Uwu")[promptKey];
+
+                if (prompt == null)
+                {
+                    await Context.Channel
+                        .SendMessageAsync("Invalid key. Couldn't find a prompt matching given key.")
+                        .ConfigureAwait(false);
+                    throw new KeyNotFoundException();
+                }
+
+                await Context.Channel.SendMessageAsync(prompt).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Error occurred during ... uwu?");
+            }
+        }
+        
+        [Command("hmm", "hmmm", "hmmmm")]
+        [Description("Toss a coin.")]
+        [CommandCooldown(1, 3, CooldownMeasure.Seconds, CooldownType.User)]
+        public async Task Command_Hmm()
+        {
+            try
+            {
+                if (!Context.BotMember.GetPermissionsFor(Context.Channel as CachedTextChannel).SendMessages) return;
+
+                await Context.Channel.SendMessageAsync("... Frick.").ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Error occurred during coin toss.");
+            }
+        }
+        
+        [Command("roll")]
+        [Description("Rolls a dice in given size.")]
+        [CommandCooldown(1, 3, CooldownMeasure.Seconds, CooldownType.User)]
+        public async Task Command_Roll(int diceSides = 6)
+        {
+            try
+            {
+                if (!Context.BotMember.GetPermissionsFor(Context.Channel as CachedTextChannel).SendMessages) return;
+
+                if (diceSides < 1)
+                {
+                    await Context.Channel
+                        .SendMessageAsync("Dice must have at least 1 side...")
+                        .ConfigureAwait(false);
+                    throw new ArithmeticException();
+                }
+                
+                int rolledNumber = new Random().Next(1, diceSides);
+
+                await Context.Channel.SendMessageAsync("Rolled a D" + diceSides + " and got " + rolledNumber + ".").ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Error occurred during coin toss.");
+            }
+        }
+    }
+    
+    
+}
--- Modules/UserModule.cs
+++ Modules/UserModule.cs
@@ -1,11 +1,16 @@
 using System;
 using System.IO;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Disqord;
+using Disqord.Bot;
+using Disqord.Events;
 using GarlicBread.Discord;
 using GarlicBread.Extensions;
 using GarlicBread.Helpers;
+using Humanizer;
+using Microsoft.Extensions.Logging;
 using Qmmands;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
@@ -18,10 +23,14 @@ using Color = Disqord.Color;
 
 namespace GarlicBread.Modules
 {
+    [Name("User")]
     public class UserModule : GarlicBreadModuleBase
     {
+        public ILogger<CoreModule> Logger { get; set; }
+        
         [Command("avatar", "av", "a", "pic", "pfp")]
         [Description("Grabs the avatar for a user.")]
+        [CommandCooldown(1, 3, CooldownMeasure.Seconds, CooldownType.User)]
         public async Task GetAvatarAsync(
             [Name("User")]
             [Description("The user who you wish to get the avatar for.")]
@@ -37,6 +46,55 @@ namespace GarlicBread.Modules
                                  $"{UrlHelper.CreateMarkdownUrl("1024", target.GetAvatarUrl(size: 1024))} | " +
                                  $"{UrlHelper.CreateMarkdownUrl("2048", target.GetAvatarUrl(size: 2048))}")
                 .Build());
+        }
+        
+        [GuildOnly]
+        [Command("userinfo", "info", "ui", "joined", "age")]
+        [Description("Displays basic user info about the mentioned user.")]
+        [CommandCooldown(1, 3, CooldownMeasure.Seconds, CooldownType.User)]
+        public async Task Command_UserInfo(CachedMember target = null)
+        {
+            target ??= Context.Member;
+
+            if (!Context.BotMember.GetPermissionsFor(Context.Channel as CachedTextChannel).SendMessages) return;
+
+            var initialMessage = await Context.Channel.SendMessageAsync("Fetching user info...").ConfigureAwait(false);
+
+            async Task Handler(MessageReceivedEventArgs emsg)
+            {
+                try
+                {
+                    var msg = emsg.Message;
+                    if (msg.Id != initialMessage.Id) return;
+
+                    var _ = initialMessage.ModifyAsync(m =>
+                    {
+                        m.Content = null;
+                        var sb = new StringBuilder()
+                            .AppendLine($":wave: **Join date:** {target.JoinedAt.Humanize()}");
+
+                        if (Context.Bot.Latency != null)
+                            sb.AppendLine(
+                                $":handshake: **Gateway:** {(int) Context.Bot.Latency.Value.TotalMilliseconds}ms");
+
+                        m.Embed = new LocalEmbedBuilder()
+                            .WithTitle($"**{target.Name}**'s info")
+                            .WithTimestamp(DateTime.Now)
+                            .WithDescription(sb.ToString())
+                            .WithFooter($"Request {Context.RequestId}")
+                            .WithColor(Context.Color)
+                            .Build();
+                    });
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError(e, "Error occurred during user info handler");
+                }
+
+                Context.Bot.MessageReceived -= Handler;
+            }
+
+            Context.Bot.MessageReceived += Handler;
         }
     }
 }
